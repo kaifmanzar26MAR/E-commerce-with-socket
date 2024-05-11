e90fd56420a305478bae037917943b77f6a840b1
Md Kaif Manzar <kaifmanzar321@gmail.com>
2024-05-11 11:56:58 +0530

    kk

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello worldu!</h1>
     </div>
   );
 };
